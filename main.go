9878af1a7bdf598f7be83b88b8a4f48bdabdf9a3
sithum-ravishka <sithumravishka1@gmail.com>
2023-12-05 13:58:13 +0530

    update merkle tree

--- main.go
+++ main.go
@@ -1,41 +1,62 @@
 package main
 
 import (
-	"fileChunk/fileRetrieve"
+	"crypto/sha256"
 	"fileChunk/fileSplit"
 	"fmt"
 )
 
+type MerkleNode struct {
+	Hash  string
+	Left  *MerkleNode
+	Right *MerkleNode
+}
+
+func calculateHash(data string) string {
+	hash := sha256.Sum256([]byte(data))
+	return fmt.Sprintf("%x", hash)
+}
+
+func buildMerkleTree(data []string) *MerkleNode {
+
+	if len(data) == 1 {
+		return &MerkleNode{Hash: calculateHash(data[0]), Left: nil, Right: nil}
+	}
+
+	mid := len(data) / 2
+	left := buildMerkleTree(data[:mid])
+	right := buildMerkleTree(data[mid:])
+	return &MerkleNode{Hash: calculateHash(left.Hash + right.Hash), Left: left, Right: right}
+}
+
+func printTree(node *MerkleNode, indent string) {
+	if node != nil {
+		fmt.Println(indent+"Hash:", node.Hash)
+		if node.Left != nil {
+			printTree(node.Left, indent+"  ")
+		}
+		if node.Right != nil {
+			printTree(node.Right, indent+"  ")
+		}
+	}
+}
+
 const (
-	chunkSize = int64(102400)
+	chunkSize = int64(502400)
 	inputFile = "data.jpg"
 )
 
 func main() {
-	// Split the input file into chunks and calculate hash values
-	chunkNames, hashValues, err := fileSplit.SplitAndHashFile(inputFile, chunkSize)
+	// Split the input file into chunks
+	chunkNames, err := fileSplit.SplitFile(inputFile, chunkSize)
 	if err != nil {
-		fmt.Println("Error splitting and hashing file:", err)
+		fmt.Println("Error splitting file:", err)
 		return
 	}
 
-	// Set the output file name for the reconstructed file
-	outputFileName := "new.jpg"
+	// fmt.Printf("Size: %v\n", name)
 
-	// Retrieve and concatenate the chunks to reconstruct the original file,
-	// verifying the integrity of each chunk using the calculated hash values
-	err = fileRetrieve.RetrieveChunksAndVerify(chunkNames, hashValues, outputFileName)
-	if err != nil {
-		fmt.Println("Error retrieving and verifying chunks:", err)
-		return
-	}
-
-	// Print hash values for each chunk
-	fmt.Println("Hash values for each chunk:")
-	for i, hashValue := range hashValues {
-		fmt.Printf("Chunk %d: %s\n", i+1, hashValue)
-	}
-
-	// Print a success message if the process completes without errors
-	fmt.Println("File split, hashed, retrieved, and verified successfully!")
+	root := buildMerkleTree(chunkNames)
+	printTree(root, "")
+	fmt.Println("Root Hash:", root.Hash)
 }
