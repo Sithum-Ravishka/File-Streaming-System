55d3fe569c19763c467fe96c4e91ee5829c7f901
sithum-ravishka <sithumravishka1@gmail.com>
2023-12-06 14:13:32 +0530

    update code

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"math/big"
@@ -58,8 +59,8 @@ func SplitFile(inputFile string, chunkSize int64) ([]string, error) {
 func main() {
 
 	const (
-		chunkSize = int64(502400)
-		inputFile = "data.jpg"
+		chunkSize = int64(256000)
+		inputFile = "test.png"
 	)
 	chunkNames, err := SplitFile(inputFile, chunkSize)
 	if err != nil {
@@ -85,6 +86,8 @@ func main() {
 		mt.Add(ctx, index, value)
 
 		fmt.Println(mt)
+		fmt.Println("Root Key after adding chunk", i+1, ":", mt.Root().String())
+
 	}
 
 	// Proof of membership of a leaf with index 1
@@ -97,6 +100,11 @@ func main() {
 	proofNotExist, _, _ := mt.GenerateProof(ctx, big.NewInt(10), mt.Root())
 
 	fmt.Println("Proof of membership:", proofNotExist.Existence)
+
+	// transform root from bytes array to json
+	claimToMarshal, _ := json.Marshal(mt.Root())
+
+	fmt.Println(string(claimToMarshal))
 }
 
 func calculateHash(chunkName string) (*big.Int, error) {
