d54219bf9ce97e5101dfed9edbbba13ec12482dd
sithum-ravishka <sithumravishka1@gmail.com>
2023-12-06 20:19:03 +0530

    create file retrieve with proof verify

--- main.go
+++ main.go
@@ -13,45 +13,80 @@ import (
 	"github.com/iden3/go-merkletree-sql/db/memory"
 )
 
-func SplitFile(inputFile *os.File, chunkSize int64) ([]int64, error) {
+func SplitFile(inputFile *os.File, chunkSize int64) ([]string, []string, error) {
 	defer inputFile.Close()
 
 	fileInfo, err := inputFile.Stat()
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	fileSize := fileInfo.Size()
+	var chunkNames []string
+	var hashValues []string
 
-	chunkSizes := make([]int64, 0)
 	hasher := sha256.New()
 
 	for i := int64(0); i < fileSize; i += chunkSize {
-		chunkFile, err := os.Create(fmt.Sprintf("%v_chunk%d", inputFile, i/chunkSize+1))
+		chunkFile, err := os.Create(fmt.Sprintf("%v_chunk%d", inputFile.Name(), i/chunkSize+1))
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 
 		multiWriter := io.MultiWriter(chunkFile, hasher)
 
 		_, err = io.CopyN(multiWriter, inputFile, chunkSize)
 		if err != nil && err != io.EOF {
-			return nil, err
+			return nil, nil, err
 		}
 
 		chunkFile.Close()
-		chunkSizes = append(chunkSizes, chunkSize)
+		chunkNames = append(chunkNames, chunkFile.Name())
 
 		hashValue := fmt.Sprintf("%x", hasher.Sum(nil))
-		newChunkName := hashValue
-		err = os.Rename(chunkFile.Name(), newChunkName)
-		if err != nil {
-			return nil, err
-		}
+		hashValues = append(hashValues, hashValue)
+
 		hasher.Reset()
 	}
 
-	return chunkSizes, nil
+	return chunkNames, hashValues, nil
+}
+
+func RetrieveChunksAndVerify(chunkNames []string, hashValues []string, outputFileName string) error {
+	outputFile, err := os.Create(outputFileName)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
+
+	hasher := sha256.New()
+
+	for i, chunkName := range chunkNames {
+		chunkFile, err := os.Open(chunkName)
+		if err != nil {
+			return err
+		}
+		defer chunkFile.Close()
+
+		// Create a multi-reader to both read from the file and calculate the hash
+		multiReader := io.TeeReader(chunkFile, hasher)
+
+		_, err = io.Copy(outputFile, multiReader)
+		if err != nil {
+			return err
+		}
+
+		// Verify the hash of the chunk
+		hashValue := fmt.Sprintf("%x", hasher.Sum(nil))
+		if hashValue != hashValues[i] {
+			return fmt.Errorf("hash verification failed for chunk %s", chunkName)
+		}
+
+		// Reset the hash for the next iteration
+		hasher.Reset()
+	}
+
+	return nil
 }
 
 func main() {
@@ -59,9 +94,11 @@ func main() {
 	inputFile, err := os.Open("data.jpg")
 	if err != nil {
 		fmt.Printf("Error: %v", err)
+		return
 	}
+	defer inputFile.Close()
 
-	chunkSizes, err := SplitFile(inputFile, chunkSize)
+	chunkNames, hashValues, err := SplitFile(inputFile, chunkSize)
 	if err != nil {
 		fmt.Println("Error splitting and hashing file:", err)
 		return
@@ -71,13 +108,19 @@ func main() {
 	store := memory.NewMemoryStorage()
 	mt, _ := merkletree.NewMerkleTree(ctx, store, 32)
 
-	for index, value := range chunkSizes {
-		mt.Add(ctx, big.NewInt(int64(index)), big.NewInt(value))
+	for index, value := range chunkNames {
+		mt.Add(ctx, big.NewInt(int64(index)), big.NewInt(0)) // You might need to adjust the second parameter based on your use case
 		fmt.Println(ctx, index, value)
 
 		// Proof of membership for each chunk
 		proofExist, _, _ := mt.GenerateProof(ctx, big.NewInt(int64(index)), mt.Root())
 		fmt.Printf("Proof of membership for chunk %d: %v\n", index, proofExist.Existence)
+
+		err := newFunction(proofExist, chunkNames, hashValues, "restored_data.jpg")
+		if err != nil {
+			fmt.Println("Error retrieving and verifying chunks:", err)
+			return
+		}
 	}
 
 	// Proof of non-membership for a non-existing chunk (e.g., index 100)
@@ -87,4 +130,11 @@ func main() {
 
 	claimToMarshal, _ := json.Marshal(mt.Root())
 	fmt.Println(string(claimToMarshal))
+}
+
+func newFunction(proofExist *merkletree.Proof, chunkNames []string, hashValues []string, outputFileName string) error {
+	if proofExist.Existence {
+		return RetrieveChunksAndVerify(chunkNames, hashValues, outputFileName)
+	}
+	return fmt.Errorf("proof of non-membership received")
 }
